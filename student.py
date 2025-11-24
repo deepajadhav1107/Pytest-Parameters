6ae0bb6efa1764a7e7637af8ec4334aa4bfcf1a7
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-24 10:54:59 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,10 @@
+import sys
+
 def student_details(name, age):
     return f"{name} is {age} years old."
 
 if __name__ == "__main__":
-    output = student_details("Rahul", 20)
-    print(output)
+    name = sys.argv[1]
+    age = sys.argv[2]
+
+    print(student_details(name, age))
