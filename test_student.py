052c994b8fb67e697e3b66f3668db979dc90c8ee
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-24 10:55:32 +0530

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,5 +1,5 @@
 from student import student_details
 
 def test_student_details():
-    result = student_details("Rahul", 20)
+    result = student_details("Rahul", "20")
     assert result == "Rahul is 20 years old."
